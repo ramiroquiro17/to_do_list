2a93e242f83beab798d83c5557af5fd61675ac91
Ramiro Quiroga <r.quiroga@itecriocuarto.org.ar>
2024-04-23 17:32:04 -0300

    agregrar fucion de borrado de tareas

--- to_do_list.js
+++ to_do_list.js
@@ -17,22 +17,27 @@ const mostrarTareas = () => {
   let html = "<ul>";
   tareas.forEach(function(elemento, indice) {
     if (elemento.completada === false) {
-      html += `<li id="tarea-${indice}">${elemento.nombre}</li>`;  
+      html += `<li id="tarea-${indice}">${elemento.nombre} <button id="boton-${indice}">Marcar como Completada</button><button id="botonEliminar-${indice}">X</button></li>`;  
     } else{
-      html += `<li id="tarea-${indice}">${elemento.nombre} COMPLETADA</li>`;
+      html += `<li id="tarea-${indice}">${elemento.nombre} COMPLETADA <button id="boton-${indice}">Marcar como completada</button><button id="botonEliminar-${indice}">X</button></li>`;
     }
   });
   html += "</ul>";
   document.getElementById("lista").innerHTML = html;
-
-  
   tareas.forEach((elemento, indice) => {
-    document.getElementById(`tarea-${indice}`).addEventListener("click", function(){
+    document.getElementById(`boton-${indice}`).addEventListener("click", function(){
       
       tareas[indice].completada = !tareas[indice].completada;
       mostrarTareas(); 
     });
   });
+  tareas.forEach((elemento,indice)=>{
+    document.getElementById(`botonEliminar-${indice}`).addEventListener("click",function(){
+      tareas.splice(indice,1)
+      mostrarTareas();
+    })
+  })
+  console.log(tareas)
 };
 
 mostrarTareas();
